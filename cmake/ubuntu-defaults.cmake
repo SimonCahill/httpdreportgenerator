120f4faa82ad7caa46bb275a3804118bba1eb24a
Simon Cahill <simon@simonc.eu>
2023-03-14 23:24:58 +0100

    Added compile-time default settings for Ubuntu-based machines (with default Apache configs)

---
+++ cmake/ubuntu-defaults.cmake
@@ -0,0 +1,7 @@
+######################################################
+##  CMake configuration file with sane defaults for ##
+##  Ubuntu systems.                                 ##
+##  ©️ Simon Cahill                                  ##
+######################################################
+
+set(httpdreport_LOG_LOCATION "/var/log/apache2")
